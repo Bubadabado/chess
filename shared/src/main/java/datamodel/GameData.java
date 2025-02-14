3c76d046950c7fa89661dd8b79aaf02d83062b92
Bubadabado <adam@adamrobertson.us>
2025-02-13 17:46:56 -0700

    add gamedata record

---
+++ shared/src/main/java/datamodel/GameData.java
@@ -0,0 +1,7 @@
+package datamodel;
+
+import chess.ChessGame;
+
+public record GameData(int GameID, String whiteUsername, String blackUsername,
+                       String gameName, ChessGame game) {
+}
