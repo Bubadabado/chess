1bb6eb6b72d579f84eec96f574b30fa500e3e0ab
Bubadabado <adam@adamrobertson.us>
2025-02-26 20:45:15 -0700

    retry auto grade

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -2,7 +2,6 @@ package service;
 import dataaccess.DataAccessException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class UserServiceTests {
     private void reset() {
