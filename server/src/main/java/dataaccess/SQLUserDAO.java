c01372882b62d2ee190b080b885dce2736f88a13
Bubadabado <adam@adamrobertson.us>
2025-03-08 10:37:04 -0700

    sqluserdao getuser

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -2,10 +2,24 @@ package dataaccess;
 
 import model.UserData;
 
+import java.sql.SQLException;
+
 public class SQLUserDAO implements UserDAO{
+    //TODO: bCrypt
     @Override
     public UserData getUser(String username) throws DataAccessException {
-        return null;
+        try (var conn = DatabaseManager.getConnection()) {
+            var query = "SELECT username, password, email FROM users" +
+                    "WHERE username = ?";
+            try (var preparedStatement = conn.prepareStatement(query)) {
+                preparedStatement.setString(1, username);
+                var rs = preparedStatement.executeQuery();
+                rs.next();
+                return new UserData(rs.getString("username"), rs.getString("password"), rs.getString("email"));
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error: failed to connect to DB on getUser");
+        }
     }
 
     @Override
