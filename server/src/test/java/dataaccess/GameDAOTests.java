43750c5d08a79d04a82d5599a9de2f7d36577ad4
Bubadabado <adam@adamrobertson.us>
2025-03-10 17:57:43 -0600

    stub game dao tests

--- server/src/test/java/dataaccess/GameDAOTests.java
+++ server/src/test/java/dataaccess/GameDAOTests.java
@@ -8,4 +8,66 @@ public class GameDAOTests {
     private void reset() {
         AdminService.clear();
     }
+
+    @Test
+    public void testCreateGameSuccess() {
+        reset();
+        var games = new SQLGameDAO();
+    }
+    @Test
+    public void testCreateGameFailure() {
+        reset();
+        var games = new SQLGameDAO();
+    }
+
+    @Test
+    public void testFindGameSuccess() {
+        reset();
+        var games = new SQLGameDAO();
+    }
+    @Test
+    public void testFindGameFailure() {
+        reset();
+        var games = new SQLGameDAO();
+    }
+
+    @Test
+    public void testJoinGameSuccess() {
+        reset();
+        var games = new SQLGameDAO();
+    }
+    @Test
+    public void testJoinGameFailure() {
+        reset();
+        var games = new SQLGameDAO();
+    }
+
+    @Test
+    public void testGetColorSuccess() {
+        reset();
+        var games = new SQLGameDAO();
+    }
+    @Test
+    public void testGetColorFailure() {
+        reset();
+        var games = new SQLGameDAO();
+    }
+
+    @Test
+    public void testListGamesSuccess() {
+        reset();
+        var games = new SQLGameDAO();
+    }
+    @Test
+    public void testListGamesFailure() {
+        reset();
+        var games = new SQLGameDAO();
+    }
+
+    @Test
+    public void testClearGamesSuccess() {
+        reset();
+        var games = new SQLGameDAO();
+        Assertions.assertDoesNotThrow(games::clearGames);
+    }
 }
