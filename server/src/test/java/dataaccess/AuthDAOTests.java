ccb8c78b9d10e4e79a519f23e02bdfc6bb00c2ea
Bubadabado <adam@adamrobertson.us>
2025-03-10 17:40:46 -0600

    test create auth

--- server/src/test/java/dataaccess/AuthDAOTests.java
+++ server/src/test/java/dataaccess/AuthDAOTests.java
@@ -1,12 +1,27 @@
 package dataaccess;
 import dataaccess.DataAccessException;
+import model.AuthData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import service.AdminService;
 
 public class AuthDAOTests {
-//    var auths = new SQLAuthDAO();
     private void reset() {
         AdminService.clear();
+    }
+
+    @Test
+    public void testCreateAuthSuccess() {
+        reset();
+        var auths = new SQLAuthDAO();
+        var expected = "super secure auth";
+        var actual = "something else";
+        try {
+            auths.createAuth(new AuthData(expected, "user"));
+            actual = auths.getAuth(expected).authToken();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        Assertions.assertEquals(expected, actual);
     }
 }
