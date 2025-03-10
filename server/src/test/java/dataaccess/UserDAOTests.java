0f08a985a7995e0bc767c9544c7b1e63f003b92f
Bubadabado <adam@adamrobertson.us>
2025-03-10 17:28:40 -0600

    get user tests

--- server/src/test/java/dataaccess/UserDAOTests.java
+++ server/src/test/java/dataaccess/UserDAOTests.java
@@ -1,5 +1,6 @@
 package dataaccess;
 import dataaccess.DataAccessException;
+import model.UserData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import service.AdminService;
@@ -11,11 +12,31 @@ public class UserDAOTests {
 
     @Test
     public void testGetUserSuccess() {
-        //TODO
+        reset();
+        var users = new SQLUserDAO();
+        var actual = new UserData("no", "no", "no");
+        try {
+            users.createUser(new UserData("user", "pwd", "email"));
+            actual = users.getUser("user");
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        var expected = new UserData("user", "pwd", "email");
+        Assertions.assertEquals(expected, actual);
     }
     @Test
     public void testGetUserFailure() {
-        //TODO
+        reset();
+        var users = new SQLUserDAO();
+        var actual = new UserData("no", "no", "no");
+        try {
+            users.createUser(new UserData("user", "pwd", "email"));
+            actual = users.getUser("name not in db");
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        String expected = null;
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
@@ -29,6 +50,7 @@ public class UserDAOTests {
 
     @Test
     public void testClearUsersSuccess() {
+        reset();
         var users = new SQLUserDAO();
         var actual = users.clearUsers();
         var expected = true;
